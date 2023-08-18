232b532100caf8f47c47724c87ba8c0abd59e575
Hongrui Fang <chfanghr@gmail.com>
2023-08-18 20:20:56 +0800

    allow `genkey` to generate many key pairs

--- bin/genkey.ts
+++ bin/genkey.ts
@@ -1,24 +1,39 @@
-import { command, option, string, run } from "cmd-ts";
-import { PrivateKey } from "snarkyjs";
+import { command, option, string, run, restPositionals, flag } from "cmd-ts";
+import { Account, Mina, PrivateKey } from "snarkyjs";
 import fs from 'fs/promises';
 
 await run(command({
   name: "genkey",
   args: {
-    output: option({
-      long: "output",
-      short: 'o',
-      defaultValue: () => "./key.json",
-      type: string,
+    fundAccounts: flag({
+      long: "fund-accounts"
     }),
+    outputs: restPositionals({
+      type: string
+    })
   },
   handler: async (args) => {
-    const privateKey = PrivateKey.random();
-    const publicKey = privateKey.toPublicKey();
-    const keyJson = JSON.stringify({
-      privateKey: privateKey.toBase58(),
-      publicKey: publicKey.toBase58()
-    });
-    await fs.writeFile(args.output, keyJson);
+    for (const output of args.outputs) {
+      console.log("generating...");
+
+      const privateKey = PrivateKey.random();
+      const publicKey = privateKey.toPublicKey();
+
+      if (args.fundAccounts) {
+        console.log("requesting funds...");
+        await Mina.faucet(publicKey);
+      }
+
+      const keyPair = {
+        privateKey: privateKey.toBase58(),
+        publicKey: publicKey.toBase58()
+      };
+
+      await fs.writeFile(output, JSON.stringify(keyPair));
+
+      console.log("done");
+    };
   }
 }), process.argv.slice(2));
+
+process.exit(0);
